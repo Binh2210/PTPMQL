3b4ddacc5043194295408886af3ae693b356a665
Binh2210 <2121050130@student.humg.edu.vn>
2025-02-20 13:20:34 +0700

    BTH_Phan5

---
+++ FirstWebMVC/Models/Student.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace FirstWebMVC.Models;
+
+    public class Student
+    {
+            public string PersonId { get; set;}
+            public string Fullname { get; set;}
+            
+
+    }
+
